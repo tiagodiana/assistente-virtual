3df7611d3ec3e66013fdfff5f19048170842c4c8
tiagodiana <tiago.diana@etec.sp.gov.br>
2020-03-02 16:00:44 -0300

    Debian Install

---
+++ install_debian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install swig cmake build-essential python3-dev python3-wheel python3-pip python3-pyaudio python3-pocketsphinx libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev
+
+sudo python3 -m pip install -r requirements_debian.txt
